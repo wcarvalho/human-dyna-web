3a6617d2b71ad5499ae2e92c6d69fd81d24d5fb2
Sam Hall-McMaster <hall-mcmaster@mpib-berlin.mpg.de>
2024-09-30 10:19:07 -0400

    add analysis folder

---
+++ analysis/1_get_files.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 12 11:35:59 2024
+
+@author: hall-mcmaster
+"""
+
+# This script loads potential data files and checks:
+    # 1. Their size
+    # 2. Whether the dataset is 'complete'
+    
+# If the dataset is 'complete' (the participant completed all trials),
+# the file is assigned a subject number of the format sub-*
+
+
+# import packages
+import os
+from os.path import join as opj
+import numpy as np
+import json
+
+
+# locate files in the data raw directory
+def list_files(directory):
+    fnames=[]
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            cfile=opj(root,file)
+            fnames.append(cfile)
+    return fnames
+
+cdir=os.getcwd()
+project_dir=os.path.dirname(cdir)
+maze_dir=os.path.dirname(project_dir)
+data_dir_raw=opj(maze_dir,'human-dyna-web-raw-data','pilot-1')
+raw_files=list_files(data_dir_raw)
+
+# create output folder for the subset of relevant files
+exp_string='pilot-1-subset'
+data_dir_output=opj(maze_dir,'human-dyna-web-raw-data', exp_string)
+os.makedirs(data_dir_output, exist_ok=True)
+
+
+# loop over files and check for 'complete' data sets
+max_stage_possible=22 # hard coded for based on specific experiment
+nfiles=len(raw_files)
+file_paths_filtered=[]
+sub_count=0
+
+for file_path in raw_files:
+    with open(file_path, 'r') as f:
+        data_dicts = json.load(f)
+        
+        # assess number of stages in data file
+        stage_idx_values = [d['stage_idx'] for d in data_dicts]
+        stage_idx_array = np.array(stage_idx_values)
+        unique_stage_idx = np.unique(stage_idx_array)
+        cmax_stage=np.max(unique_stage_idx)
+        if cmax_stage>max_stage_possible:
+            max_stage=cmax_stage
+            
+        # if the number of changes are as expected, retain the file
+        if cmax_stage>=max_stage_possible:
+            file_paths_filtered.append(file_path)
+            
+            # update the filename and save (e.g. sub-01)
+            sub_count+=1
+            sub_string = f'sub-{sub_count:02}' + '.json'
+            fname_new=opj(data_dir_output,sub_string)
+            
+            with open(fname_new, 'w') as file:
+                json.dump(data_dicts, file)
+            
+    
+
+
---
+++ experiment_2.py
@@ -0,0 +1,300 @@
+from functools import partial
+
+from housemaze import renderer
+from housemaze.maze import KeyboardActions
+from housemaze.human_dyna import env
+from housemaze.human_dyna import utils
+from housemaze.human_dyna import env as maze
+from housemaze.human_dyna import mazes
+
+import jax
+import jax.numpy as jnp
+from flax import struct
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+DEBUG = int(os.environ.get('DEBUG', 0))
+
+from nicegui import ui
+from nicewebrl.stages import Stage, EnvStage, make_image_html
+from nicewebrl.nicejax import JaxWebEnv, base64_npimage
+
+num_rooms = 3          # number of pairs to recognize interaction for
+num_pairs_for_exp = 1  # number of objects to be train
+char2key, group_set, task_objects = mazes.get_group_set(num_rooms)
+image_data = utils.load_image_dict()
+
+task_runner = env.TaskRunner(task_objects=task_objects)
+keys = image_data['keys']
+
+jax_env = env.HouseMaze(
+    task_runner=task_runner,
+    num_categories=len(keys),
+    use_done=True,
+)
+jax_env = utils.AutoResetWrapper(jax_env)
+
+
+def make_train_params(maze_str):
+  return mazes.get_maze_reset_params(
+      group_set=group_set[:num_pairs_for_exp],
+      char2key=char2key,
+      maze_str=maze_str,
+      label=jnp.array(0),
+      make_env_params=True,
+  ).replace(
+        terminate_with_done=jnp.array(2),
+  )
+
+
+def render_fn(timestep: maze.TimeStep) -> jnp.ndarray:
+    image = renderer.create_image_from_grid(
+        timestep.state.grid,
+        timestep.state.agent_pos,
+        timestep.state.agent_dir,
+        image_data)
+    return image
+
+render_fn = jax.jit(render_fn)
+
+action_to_key = {
+    int(KeyboardActions.right): "ArrowRight",
+    int(KeyboardActions.down): "ArrowDown",
+    int(KeyboardActions.left): "ArrowLeft",
+    int(KeyboardActions.up): "ArrowUp",
+    int(KeyboardActions.done): "d",
+}
+
+action_to_name = {
+    int(KeyboardActions.right): "right",
+    int(KeyboardActions.down): "down",
+    int(KeyboardActions.left): "left",
+    int(KeyboardActions.up): "up",
+    int(KeyboardActions.done): "done",
+}
+
+web_env = JaxWebEnv(jax_env)
+# Call this function to pre-compile jax functions before experiment starsts.
+dummy_env_params = make_train_params(mazes.maze0)
+web_env.precompile(dummy_env_params=dummy_env_params)
+vmap_render_fn = web_env.precompile_vmap_render_fn(
+    render_fn, dummy_env_params)
+
+
+def evaluate_success_fn(timestep):
+    return int(timestep.reward > .5)
+
+
+@struct.dataclass
+class EnvStageState:
+    timestep: maze.TimeStep = None
+    nsteps: int = 0
+    nepisodes: int = 0
+    nsuccesses: int = 0
+
+
+def stage_display_fn(stage, container):
+    with container.style('align-items: center;'):
+        container.clear()
+        ui.markdown(f"## {stage.name}")
+        ui.markdown(f"{stage.body}")
+
+def env_stage_display_fn(
+        stage,
+        container,
+        timestep):
+    image = render_fn(timestep)
+    image = base64_npimage(image)
+    category = keys[timestep.state.task_object]
+
+    stage_state = stage.get_user_data('stage_state')
+    with container.style('align-items: center;'):
+        container.clear()
+        ui.markdown(f"## {stage.name}")
+        #ui.markdown(f"#### {stage.instruction}")
+        ui.markdown(f"#### Please obtain the {category}")
+        with ui.row():
+            with ui.element('div').classes('p-2 bg-blue-100'):
+                ui.label().bind_text_from(
+                    stage_state, 'nsuccesses', lambda n: f"Number of successful episodes: {n}/{stage.min_success}")
+            with ui.element('div').classes('p-2 bg-green-100'):
+                ui.label().bind_text_from(
+                    stage_state, 'nepisodes', lambda n: f"Try: {n}/{stage.max_episodes}")
+
+        text = f"You must complete at least {stage.min_success} episodes. You have {stage.max_episodes} tries."
+        ui.html(text).style('align-items: center;')
+        ui.html(make_image_html(src=image))
+
+
+def make_env_stage(
+        name,
+        maze_str,
+        max_episodes=1,
+        min_success=1,
+        training=True):
+    return EnvStage(
+        name=name,
+        instruction='Please obtain the goal object',
+        web_env=web_env,
+        action_to_key=action_to_key,
+        action_to_name=action_to_name,
+        env_params=make_train_params(maze_str).replace(
+          training=False,
+          # if training sample_train=1.0
+          # if testing, sample_train=0.0
+          p_test_sample_train=float(training),
+          ),
+        render_fn=render_fn,
+        vmap_render_fn=vmap_render_fn,
+        display_fn=env_stage_display_fn,
+        evaluate_success_fn=evaluate_success_fn,
+        state_cls=EnvStageState,
+        max_episodes=1 if DEBUG else max_episodes,
+        min_success=1 if DEBUG else min_success,
+    )
+
+stages = [
+    Stage(
+        name='Experiment instructions',
+        body="""
+        This is an experiment to test a person's ability to remember how to do a task that they could have done but didn't do.
+
+        In the experiment, you'll control a red triangle in a maze. 
+
+        During training you'll need to get an object. At evaluation, we'll test your ability to get a different object.
+
+        Press the button when you're ready to begin.
+        """,
+        display_fn=stage_display_fn,
+    ),
+]
+#############
+# Practice
+#############
+maze1 = """
+.#.C...##....
+.#..D...####.
+.######......
+......######.
+.#.#..#......
+.#.#.##..#...
+##.#.#>.###.#
+A..#.##..#...
+.B.#.........
+#####.#..####
+......####.#.
+.######E.#.#.
+........F#...
+""".strip()
+
+stages.extend([
+    Stage(
+        name='Practice training',
+        body="""
+        Here you'll practice learning a task in the environment.
+
+        You can control the red triangle with the arrow keys on your keyboard.
+        Your goal is to move it to the goal object. 
+
+        Press the button when you're ready to continue.
+        """,
+        display_fn=stage_display_fn,
+    ),
+    make_env_stage(
+        'Practice',
+        maze_str=maze1,
+        min_success=2,
+        max_episodes=5,
+        training=True),
+    Stage(
+        name='Practice evaluation',
+        body="""
+        Here you'll experience the task of getting an object that was nearby the other object you could have achieved.
+
+        During the evaluation phase, you will only have 1 try. If you get this stage correct, you will double your earnings.
+
+        Press the button when you're ready to continue.
+        """,
+        display_fn=stage_display_fn,
+    ),
+    make_env_stage(
+        'Practice',
+        maze_str=mazes.maze1,
+        min_success=1,
+        max_episodes=1,
+        training=False),
+])
+"""
+Experiments Will be
+1. Shortcut introduction (need to higlight open sections somehow)
+  - train: maze3
+  - test1: maze3_open2
+2. Change starting location (need to highlight map changes)
+  - test: maze3_onpath
+  - test: maze3_onpath_shortcut
+  - test: maze3_offpath_shortcut
+
+3. closer/further object:
+  - train: maze5
+  - test: maze5
+"""
+stages.extend([
+    Stage(
+        name='Training on Maze 1',
+        body="""
+        Please learn to obtain the objects. You need to succeed 20 times.
+        """,
+        display_fn=stage_display_fn,
+    ),
+    make_env_stage(
+        'Maze 1', maze_str=mazes.maze3,
+        min_success=20, max_episodes=30, training=True),
+    Stage(
+        name='Evaluation on Maze 1',
+        body="""
+        The following are evaluaton tasks. You will get 1 chance each time.
+
+        **Note that some parts of the maze may have changed**.
+        """,
+        display_fn=stage_display_fn,
+    ),
+    #make_env_stage(
+    #    'Maze 1', 'maze3_open',
+    #    min_success=1, max_episodes=1, training=False),
+    make_env_stage(
+        'Maze 1', maze_str=mazes.maze3_open2,
+        min_success=1, max_episodes=1, training=False),
+    make_env_stage(
+        'Maze 1', maze_str=mazes.maze3_onpath,
+        min_success=1, max_episodes=1, training=False),
+    make_env_stage(
+        'Maze 1', maze_str=mazes.maze3_onpath_shortcut,
+        min_success=1, max_episodes=1, training=False),
+    make_env_stage(
+        'Maze 1', maze_str=mazes.maze3_offpath_shortcut,
+        min_success=1, max_episodes=1, training=False),
+])
+
+stages.extend([
+    Stage(
+        name='Training on Maze 2',
+        body="""
+        Please learn to obtain the objects. You need to succeed 20 times.
+        """,
+        display_fn=stage_display_fn,
+    ),
+    make_env_stage(
+        'Maze 1', maze_str=mazes.maze5,
+        min_success=20, max_episodes=30, training=True),
+    Stage(
+        name='Evaluation on Maze 2',
+        body="""
+        The following are evaluaton tasks. You will get 1 chance each time.
+        """,
+        display_fn=stage_display_fn,
+    ),
+    make_env_stage(
+        'Maze 1', maze_str=mazes.maze5,
+        min_success=1, max_episodes=1, training=False),
+])
